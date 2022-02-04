b0a1cab9a42bff1ca142321d77a6407c7eb388eb
Jon Lighthall <jon.lighthall@gmail.com>
2022-02-04 14:42:54 -0600

    this time updating copy from vs code, update to ps commands

--- copy_and_rename.ps1
+++ copy_and_rename.ps1
@@ -1,27 +1,27 @@
-$userdir = 'C:\Users\jlighthall'
-$source = '$userdir\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\'
-$target = '$userdir\Pictures'
+userdir = 'C:\Users\jlighthall'
+source = '$userdir\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\'
+target = '$userdir\Pictures'
 
-echo "copy pictures..."
+Write-Output "copy pictures..."
 if (Test-Path -Path $source) {
-       echo "$source found"
+       Write-Output "$source found"
        if (Test-Path -Path $target) {
-	      echo "$target found"
-	      echo "proceeding with copy..."
-	      cp $source\* $target
-	      cd $target
+	      Write-Output "$target found"
+	      Write-Output "proceeding with copy..."
+	      Copy-Item $source\* $target
+	      Set-Location $target
 	      Get-ChildItem -Exclude *.jpg,*.ps1 | Rename-Item -NewName { $_.Name + ".jpg"} | Remove-Item
 	      Get-ChildItem -Exclude *.jpg,*.ps1 | Remove-Item
-	      echo "done"
+	      Write-Output "done"
 	  }
    else {
-    echo "$target not found"
-    echo "no destination to copy to`nexiting"
+    Write-Output "$target not found"
+    Write-Output "no destination to copy to`nexiting"
     }   	      
    }
    else {
-    echo "$source not found"
-    echo "no pictures to copy`nexiting"
+    Write-Output "$source not found"
+    Write-Output "no pictures to copy`nexiting"
     }   	      
 	      
 
