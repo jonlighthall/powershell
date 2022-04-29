569c622607768ee67eb51bb532ce36083b9d628f
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-29 06:47:44 -0500

    FIX copy, add return to start dir

--- copy_and_rename.ps1
+++ copy_and_rename.ps1
@@ -1,6 +1,7 @@
 Set-Variable -Name "userdir" -Value "C:\Users\jlighthall"
 Set-Variable -Name "source" -Value "$userdir\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
 Set-Variable -Name "target" -Value "$userdir\OneDrive - US Navy-flankspeed\Pictures\Lock Screen"
+$start_dir = pwd
 
 Write-Output "copy pictures..."
 if (Test-Path -Path $source) {
@@ -8,11 +9,12 @@ if (Test-Path -Path $source) {
        if (Test-Path -Path $target) {
 	      Write-Output "$target found"
 	      Write-Output "proceeding with copy..."
-	      Copy-Item $source\*.jpg $target
+	      Copy-Item -Verbose $source\* $target
 	      Set-Location $target
 	      Get-ChildItem -Exclude *.jpg,*.ps1 | Rename-Item -NewName { $_.Name + ".jpg"} | Remove-Item
 	      Get-ChildItem -Exclude *.jpg,*.ps1 | Remove-Item
 	      Write-Output "done"
+          Set-Location $start_dir
 	  }
    else {
     Write-Output "$target not found"
