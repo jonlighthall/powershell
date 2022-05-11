f99b94dddaa5a642f7e3208169a4f22cef84d906
Jon Lighthall <jon.lighthall@gmail.com>
2022-05-11 11:07:17 -0500

    add env, add wait for process

--- blank.ps1
+++ blank.ps1
@@ -4,9 +4,23 @@
 
 Switch-Desktop -Desktop 1
 $office_dir = 'C:\Program Files (x86)\Microsoft Office\Office16'
-$ppt_dir = 'C:\Users\jlighthall\OneDrive\Desktop'
+$ppt_dir = '$Env:OneDrive\Desktop'
 $ppt_name = 'blank.ppsx'
 Write-Output "starting presentation $ppt_name..." 
-Start-Process -WorkingDirectory $office_dir -FilePath .\POWERPNT.EXE -ArgumentList "/S `"$ppt_dir\$ppt_name`"" 
-Start-Sleep -Milliseconds 1000 # add wait to ensure ppt opens on other desktop
+Start-Process -WorkingDirectory $office_dir -FilePath .\POWERPNT.EXE -ArgumentList "/S `"$ppt_dir\$ppt_name`""
+$StartTime = $(get-date)
+$check = Get-Process POWERPNT -ErrorAction SilentlyContinue
+$wait_ms=100
+
+# wait for CPU to increase before switching back
+while (($check).CPU -lt 1.0) {
+ {Write-Output "still loading..."}
+ Start-Sleep -Milliseconds $wait_ms
+ Write-Output "CPU = " ($check).CPU
+ $elapsedTime = $(get-date) - $StartTime
+ Write-Output "elapsed time  = $elapsedTime" 
+}
+
+# switch back to primary desktop
+Start-Sleep -Milliseconds wait_ms
 Switch-Desktop -Desktop 0
