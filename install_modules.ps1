e12b38934608dad18837e8a39fb27190b0edfa4b
Jon Lighthall <jon.lighthall@gmail.com>
2022-06-24 09:50:28 -0500

    add verbose, comment out net service

--- install_modules.ps1
+++ install_modules.ps1
@@ -1,6 +1,6 @@
 # The following commands must be run before running blank.ps1
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
-Install-Module PowerShellGet -AllowClobber -Force -Scope CurrentUser
-Install-Module -Name VirtualDesktop -Scope CurrentUser -Force
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force -Verbose
+#[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -Verbose
+Install-Module PowerShellGet -AllowClobber -Force -Scope CurrentUser -Verbose
+Install-Module -Name VirtualDesktop -Scope CurrentUser -Force -Verbose
