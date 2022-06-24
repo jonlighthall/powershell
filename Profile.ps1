16681a0635818a670296748b3f73083b8203b3eb
Jon Lighthall <jon.lighthall@gmail.com>
2022-06-24 09:52:35 -0500

    add execution policy in comment

--- Profile.ps1
+++ Profile.ps1
@@ -1,2 +1,3 @@
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force -Verbose
 Write-Output "Weclome to $env:COMPUTERNAME"
 $env:PSModulePath = $env:PSModulePath + ";${HOME}\Documents\WindowsPowerShell\Modules"
