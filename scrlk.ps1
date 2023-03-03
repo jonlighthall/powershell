8f0cc456f6d6d12706f14500fc5d5811882811c4
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-03 14:57:24 -0600

    working blinking lights

--- scrlk.ps1
+++ scrlk.ps1
@@ -1,37 +1,23 @@
-$blink_wait_ms = 128
-
-Write-Output "Press Ctrl-C to exit."
-
+$blink_wait_ms = 200
 $WShell = New-Object -ComObject Wscript.Shell
-
-$keys=@("CAPSLOCK","SCROLLLOCK")
+$keys=@("{CAPSLOCK}","{SCROLLLOCK}","{NUMLOCK}")
 $nkeys=$keys.Length
-echo "$keys has length $nkeys"
-
-for ($i=0;$i -lt $nkeys;$i++) {
-echo "$i "$keys[$i]
-#$keys[$i]
-
-$blink_wait_ms
-}
-
-
-
-while ($true) {
-
-
+Write-Output "$keys has length $nkeys"
+Write-Output "start for loops"
+for ($j=0;$j -lt 10;$j++) {
+    Write-Output "outer loop $j"
     for ($i=0;$i -lt $nkeys;$i++) {
-        echo "$i "$keys[$i]
-        #$keys[$i]
-
+        Write-Output "innter loop $i"
+        Write-Output "$i "$keys[$i]
         $WShell.sendkeys("$keys[$i]")
         Start-Sleep -Milliseconds $blink_wait_ms
         $WShell.sendkeys("$keys[$i]")
-
-
-        }
-
-
+        Start-Sleep -Milliseconds $blink_wait_ms
+    }
+}
+Write-Output "start while loop"
+Write-Output "Press Ctrl-C to exit."
+while ($true) {
     $WShell.sendkeys("{SCROLLLOCK}")
     Start-Sleep -Milliseconds 100
     $WShell.sendkeys("{SCROLLLOCK}")
