e9990bdf784c72545b27456397e4bfe07559f5ac
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-03 14:43:38 -0600

    add keys variable

--- scrlk.ps1
+++ scrlk.ps1
@@ -1,8 +1,37 @@
+$blink_wait_ms = 128
+
 Write-Output "Press Ctrl-C to exit."
 
 $WShell = New-Object -ComObject Wscript.Shell
 
+$keys=@("CAPSLOCK","SCROLLLOCK")
+$nkeys=$keys.Length
+echo "$keys has length $nkeys"
+
+for ($i=0;$i -lt $nkeys;$i++) {
+echo "$i "$keys[$i]
+#$keys[$i]
+
+$blink_wait_ms
+}
+
+
+
 while ($true) {
+
+
+    for ($i=0;$i -lt $nkeys;$i++) {
+        echo "$i "$keys[$i]
+        #$keys[$i]
+
+        $WShell.sendkeys("$keys[$i]")
+        Start-Sleep -Milliseconds $blink_wait_ms
+        $WShell.sendkeys("$keys[$i]")
+
+
+        }
+
+
     $WShell.sendkeys("{SCROLLLOCK}")
     Start-Sleep -Milliseconds 100
     $WShell.sendkeys("{SCROLLLOCK}")
