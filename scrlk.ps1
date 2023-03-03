8d509f803741646c557869cb7ca3f1750c4fb43a
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-03 15:46:16 -0600

    rearrange lines

--- scrlk.ps1
+++ scrlk.ps1
@@ -1,10 +1,10 @@
 $blink_wait_ms = 32
-$blinks_per_loop = 4
-$loop_wait_min = 3/60
+$blinks_per_loop = 2
+$loop_wait_min = 2/60
 $loop_wait_s = $loop_wait_min*60
-$WShell = New-Object -ComObject Wscript.Shell
 $keys=@("{CAPSLOCK}","{SCROLLLOCK}","{NUMLOCK}")
 $nkeys=$keys.Length
+$WShell = New-Object -ComObject Wscript.Shell
 Write-Output "Press Ctrl-C to exit."
 while ($true) {
     for ($j=0;$j -lt ($blinks_per_loop*2);$j++) {
