45aaaa9def58c88be653759e101ab1c8f8d8063d
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-03 15:06:58 -0600

    working with variables

--- scrlk.ps1
+++ scrlk.ps1
@@ -4,22 +4,19 @@ $keys=@("{CAPSLOCK}","{SCROLLLOCK}","{NUMLOCK}")
 $nkeys=$keys.Length
 Write-Output "$keys has length $nkeys"
 Write-Output "start for loops"
-for ($j=0;$j -lt 10;$j++) {
-    Write-Output "outer loop $j"
-    for ($i=0;$i -lt $nkeys;$i++) {
-        Write-Output "innter loop $i"
-        Write-Output "$i "$keys[$i]
-        $WShell.sendkeys("$keys[$i]")
-        Start-Sleep -Milliseconds $blink_wait_ms
-        $WShell.sendkeys("$keys[$i]")
-        Start-Sleep -Milliseconds $blink_wait_ms
-    }
+for ($i=0;$i -lt $nkeys;$i++) {
+    Write-Output "$i $($keys[$i])"
 }
 Write-Output "start while loop"
 Write-Output "Press Ctrl-C to exit."
 while ($true) {
-    $WShell.sendkeys("{SCROLLLOCK}")
-    Start-Sleep -Milliseconds 100
-    $WShell.sendkeys("{SCROLLLOCK}")
+    for ($j=0;$j -lt 10;$j++) {
+        for ($i=0;$i -lt $nkeys;$i++) {
+            $WShell.sendkeys("$keys[$i]")
+            Start-Sleep -Milliseconds $blink_wait_ms
+            $WShell.sendkeys("$keys[$i]")
+            Start-Sleep -Milliseconds $blink_wait_ms
+        }
+    }
     Start-Sleep -Seconds 2
 }
