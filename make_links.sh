be7112c0ced93e0f6c8361e502b89f1132200dd4
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-15 10:20:03 -0500

    add testing to mklink

--- make_links.sh
+++ make_links.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
-mkdir -pv ~/bin
+SRCDIR=$PWD
+TGTDIR=$HOME/bin
+
+# check target directory
+echo -n "target directory $TGTDIR... "
+if [ -d $TGTDIR ]; then
+    echo "exists"
+else
+    echo "does not exist"
+    mkdir -pv $TGTDIR
+fi
+
 # list files to be linked in bin
 for prog in clicker
 do
-    if [ ! -f $HOME/bin/$prog ]; then
-	if [ -f $PWD/$prog.sh ]; then
-	    ln -svf $PWD/$prog.sh $HOME/bin/$prog
+    echo -n "program $SRCDIR/$prog.sh... "
+    if [ -e $SRCDIR/$prog.sh ]; then
+	echo -n "exists and is "
+	if [ -x $SRCDIR/$prog.sh ]; then
+	    echo "executable"
+
+	    echo -n "$TGTDIR/${prog}... "
+	    if [ -e $TGTDIR/${prog} ] ; then
+		echo "already exists"
+
+		if [[ $SRCDIR/$prog.sh -ef $TGTDIR/$prog ]]; then
+
+		    echo " already points to ${prog}..."
+		    echo "skipping ..."
+		    break
+		else
+		    echo " Backing up ${prog}..."
+		    mv -v $TGTDIR/${prog} $TGTDIR/${prog}_$(date +'%Y-%m-%d-t%H%M')
+		fi
+	    else
+		echo "does not exist"
+	    fi
+	    echo "making link..."
+	    ln -svf $SRCDIR/$prog.sh $TGTDIR/$prog
+	else
+	    echo "not executable"
 	fi
+    else
+	echo "does not exist"
     fi
 done
