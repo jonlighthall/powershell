fbbf59c613c9bf0f55430bdb52048465be859894
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-21 11:56:43 -0500

    organize, add dots per line

--- scrlk.ps1
+++ scrlk.ps1
@@ -1,12 +1,29 @@
+# blink settings
 $blink_wait_ms = 32
 $blinks_per_loop = 2
-$loop_wait_min = 8
-$loop_wait_s = $loop_wait_min*60
 $keys=@("{CAPSLOCK}","{SCROLLLOCK}","{NUMLOCK}")
 $nkeys=$keys.Length
-$WShell = New-Object -ComObject Wscript.Shell
+
+# loop settings
+$loop_wait_min = 8
+$loop_wait_s = $loop_wait_min*60
+$loops_per_hour = $([int](60/$loop_wait_min))
+
+# print settings
+$line_lim=10
+if ($loops_per_hour -gt $line_lim) {
+    $ndots=$line_lim
+}
+else {
+    $ndots=$loops_per_hour
+}
 Write-Output "Press Ctrl-C to exit."
 $counter = 0
+$counter2 = 0
+Write-Host "loops per hour = $loops_per_hour"
+Write-Host "dots per line = $ndots"
+
+$WShell = New-Object -ComObject Wscript.Shell
 while ($true) {
     for ($j=0;$j -lt ($blinks_per_loop*2);$j++) {
         for ($i=0;$i -lt $nkeys;$i++) {
@@ -14,10 +31,13 @@ while ($true) {
         }
         Start-Sleep -Milliseconds $blink_wait_ms
     }
-    Write-Host -NoNewline "."
-    $counter++
-    if (($counter % [int](60/$loop_wait_min))-eq 0) {
-        Write-Host "$(Get-Date -Format HH:mm)"
+    if (($counter -gt 0) -and (($counter % $ndots) -eq 0)) {
+        Write-Host " $(Get-Date -Format HH:mm)"
+	$counter2=0
     }
+    $counter2++
+    #Write-Host -NoNewline "$counter2"
+        Write-Host -NoNewline "."	
+    $counter++
     Start-Sleep -Seconds $loop_wait_s
 }
