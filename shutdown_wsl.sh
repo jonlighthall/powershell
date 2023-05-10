152e26c0e138aef30296f0815b3053e7bf08e161
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-10 10:21:43 -0500

    add write to hist file

--- shutdown_wsl.sh
+++ shutdown_wsl.sh
@@ -12,7 +12,13 @@ fi
 src_dir=$(dirname $src_name)
 cd $src_dir
 echo
+hist_file=~/.bash_history
+if [ -f $hist_file ]; then
+     echo "#$(date +'%s') SHUTDN $(date +'%a %b %d %Y %R:%S %Z') from $(hostname -s)" >> $hist_file
+fi
+echo -n "connecting to powershell... "
+powershell.exe Write-Output "done"
+echo
 powershell.exe wsl -l -v | sed 's/^/   /'
 echo -e "\nshutting down wsl.exe...\n"
-#powershell.exe -File ./shutdown_wsl.ps1 &
 powershell.exe wsl --shutdown
