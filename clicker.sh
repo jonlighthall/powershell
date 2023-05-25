50fc7f6861046cbf6551a35c3f940ece900e899e
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-25 13:57:48 -0500

    add formatting and change src dir name

--- clicker.sh
+++ clicker.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+
+# load formatting
+fpretty=${HOME}/utils/bash/.bashrc_pretty
+if [ -e $fpretty ]; then
+    source $fpretty
+fi
+
 # print source name at start
 echo "${TAB}running $BASH_SOURCE..."
 src_name=$(readlink -f $BASH_SOURCE)
-if [ ! $BASH_SOURCE = $src_name ]; then
+if [ ! "$BASH_SOURCE" = "$src_name" ]; then
     echo -e "${TAB}${VALID}link${NORMAL} -> $src_name"
 fi
+
 # get source directory
-src_dir=$(dirname $src_name)
-cd $src_dir
+source_dir=$(dirname $src_name)
+cd $source_dir
 powershell.exe -File ./scrlk.ps1
 trap "echo ' $(sec2elap $SECONDS)'" EXIT
