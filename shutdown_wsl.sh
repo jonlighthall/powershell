9054cf7bff4e6b54d3f0cbc0e0aa3bebe1215a9e
Jon Lighthall <jon.lighthall@gmail.com>
2023-11-30 19:09:53 -0600

    merge writes to one line

--- shutdown_wsl.sh
+++ shutdown_wsl.sh
@@ -27,6 +27,5 @@ if [ -f $hist_file ]; then
     echo "#$(date +'%s') SHUTDN $(date +'%a %b %d %Y %R:%S %Z') from $(hostname -s)" >>$hist_file
 fi
 echo -n "connecting to powershell... "
-powershell.exe Write-Output "done"
-powershell.exe Write-Host "shutting down WSL..."
+powershell.exe 'Write-Output "done"; Write-Host "shutting down WSL..."'
 powershell.exe -Command "wsl --shutdown"
