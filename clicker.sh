12349fc4411540c128537e645aca745c60d763f3
Jon Lighthall <jon.lighthall@gmail.com>
2024-01-29 11:27:44 -0600

    add clear line to clicker

--- clicker.sh
+++ clicker.sh
@@ -28,4 +28,4 @@ echo -n "connecting to powershell... "
 powershell.exe Write-Output "done"
 clear -x
 powershell.exe -File ./scrlk.ps1
-trap "echo -e '\x1B[11G BASH: $(sec2elap $SECONDS)'" EXIT
+trap "echo -e '\x1B[11G BASH: $(sec2elap $SECONDS)';clear -x" EXIT
