ed9d5eff3f4f8a074e13cf3b9d6a1de13949f72e
Jon Lighthall <jon.lighthall@gmail.com>
2024-01-29 11:28:09 -0600

    reset window title on scroll lock (and format)

--- scrlk.ps1
+++ scrlk.ps1
@@ -49,73 +49,74 @@ $counter = 0
 $txt = "All work and no play makes Jack a dull boy."
 
 try {
-while ($true) {
-    if ($src_pid -gt 0) {
-        $null = (New-Object -ComObject WScript.Shell).AppActivate($src_pid)
-        $do_text = $true  
-    }
-    else {
-        $do_text = $false 
-    }
-    if ($counter -gt 0) {        
-        Write-Host -NoNewline -ForegroundColor Red "$($PSStyle.bold)$msg$($PSStyle.BoldOff)"        
-        if ($do_text) {
-            $WShell.sendkeys("$txt")
+    while ($true) {
+        if ($src_pid -gt 0) {
+            $null = (New-Object -ComObject WScript.Shell).AppActivate($src_pid)
+            $do_text = $true  
         }
-        for ($j = 0; $j -lt ($blinks_per_loop * 2); $j++) {
-            for ($i = 0; $i -lt $nkeys; $i++) {                
-                $WShell.sendkeys($($keys[$i]))
-            }            
-            Start-Sleep -Milliseconds $blink_wait_ms
+        else {
+            $do_text = $false 
         }
+        if ($counter -gt 0) {        
+            Write-Host -NoNewline -ForegroundColor Red "$($PSStyle.bold)$msg$($PSStyle.BoldOff)"        
+            if ($do_text) {
+                $WShell.sendkeys("$txt")
+            }
+            for ($j = 0; $j -lt ($blinks_per_loop * 2); $j++) {
+                for ($i = 0; $i -lt $nkeys; $i++) {                
+                    $WShell.sendkeys($($keys[$i]))
+                }            
+                Start-Sleep -Milliseconds $blink_wait_ms
+            }
         
-        if ($do_text) {
-            #Start-Sleep -Milliseconds 500        
-            $WShell.sendkeys($("{BS}" * $txt.Length))
+            if ($do_text) {
+                #Start-Sleep -Milliseconds 500        
+                $WShell.sendkeys($("{BS}" * $txt.Length))
+            }
+            # clear wait message
+            $cur_pos = $host.UI.RawUI.CursorPosition;
+            $cur_pos.X -= $msg.Length;      
+            $host.UI.RawUI.CursorPosition = $cur_pos                
+            Write-Host -NoNewline $(" " * $msg.Length)
+            # reset cursor position
+            $cur_pos = $host.UI.RawUI.CursorPosition
+            $cur_pos.X -= $msg.Length;
+            $host.UI.RawUI.CursorPosition = $cur_pos                
         }
-        # clear wait message
-        $cur_pos = $host.UI.RawUI.CursorPosition;
-        $cur_pos.X -= $msg.Length;      
-        $host.UI.RawUI.CursorPosition = $cur_pos                
-        Write-Host -NoNewline $(" " * $msg.Length)
-        # reset cursor position
-        $cur_pos = $host.UI.RawUI.CursorPosition
-        $cur_pos.X -= $msg.Length;
-        $host.UI.RawUI.CursorPosition = $cur_pos                
-    }
     
-    if (($counter % $ndots) -eq 0) {
-        if ($counter -gt 0) {	                                
-            $elapsedTime = $(get-date) - $StartTime
-            if ($elapsedTime.TotalSeconds -lt 1) {
-                Write-Host " elapsed time = $("{0,5:n1}" -f $($elapsedTime.TotalMilliseconds)) ms"
+        if (($counter % $ndots) -eq 0) {
+            if ($counter -gt 0) {	                                
+                $elapsedTime = $(get-date) - $StartTime
+                if ($elapsedTime.TotalSeconds -lt 1) {
+                    Write-Host " elapsed time = $("{0,5:n1}" -f $($elapsedTime.TotalMilliseconds)) ms"
+                }
+                elseif ($elapsedTime.TotalSeconds -lt 60) {
+                    Write-Host " elapsed time = $("{0,4:n1}" -f $($elapsedTime.TotalSeconds)) s"
+                }
+                elseif ($elapsedTime.TotalMinutes -lt 60) {
+                    Write-Host " elapsed time = $("{0,4:n1}" -f $($elapsedTime.TotalMinutes)) min"    
+                }
+                else {
+                    Write-Host " elapsed time = $("{0:n1}" -f $($elapsedTime.TotalHours)) hr"    
+                }
+                if ($elapsedTime.TotalHours -ge 10) {               
+                    $cur_pos = $host.UI.RawUI.CursorPosition
+                    $cur_pos.X = 16 - 3
+                    $host.UI.RawUI.CursorPosition = $cur_pos
+                    Write-Host "PS: elapsed time exceeds 10 hr"                        
+                    exit
+                }                
             }
-            elseif ($elapsedTime.TotalSeconds -lt 60) {
-                Write-Host " elapsed time = $("{0,4:n1}" -f $($elapsedTime.TotalSeconds)) s"
-            }
-            elseif ($elapsedTime.TotalMinutes -lt 60) {
-                Write-Host " elapsed time = $("{0,4:n1}" -f $($elapsedTime.TotalMinutes)) min"    
-            }
-            else {
-                Write-Host " elapsed time = $("{0:n1}" -f $($elapsedTime.TotalHours)) hr"    
-            }
-            if ($elapsedTime.TotalHours -ge 10) {               
-                $cur_pos = $host.UI.RawUI.CursorPosition
-                $cur_pos.X = 16 - 3
-                $host.UI.RawUI.CursorPosition = $cur_pos
-                Write-Host "PS: elapsed time exceeds 10 hr"                        
-                exit
-            }                
+            Write-Host -NoNewline "$(Get-Date -Format HH:mm) "
         }
-        Write-Host -NoNewline "$(Get-Date -Format HH:mm) "
+        Write-Host -NoNewline "."	
+        $counter++
+        $this_wait = Get-Random -Minimum 0 -Maximum $loop_wait_ms
+        $this_wait = $([int] $this_wait)    
+        Start-Sleep -Milliseconds $this_wait
     }
-    Write-Host -NoNewline "."	
-    $counter++
-    $this_wait = Get-Random -Minimum 0 -Maximum $loop_wait_ms
-    $this_wait = $([int] $this_wait)    
-    Start-Sleep -Milliseconds $this_wait
-}
 }
 finally {
-	Write-Host -NoNewLine "`a"
+    Write-Host -NoNewLine "`a"    
+    $host.ui.RawUI.WindowTitle = "Ubuntu"    
 }
