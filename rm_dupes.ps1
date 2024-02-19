c40a23684075351587cd261c6d84834f65c26c32
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-19 16:10:08 -0600

    fix quotes

--- rm_dupes.ps1
+++ rm_dupes.ps1
@@ -1,4 +1,4 @@
 Get-ChildItem -Recurse | ForEach-Object {
-		$NewName = $_.Name -replace ’ \(1\)’,’’
+		$NewName = $_.Name -replace ' \(1\)',''
 		if (-not ($_.name -eq $newname)){
-			   Rename-Item -Path $_.fullname -newname ($newName) }} -Verbose; Get-ChildItem ’* (1).*’| Remove-Item -Verbose;
+			   Rename-Item -Path $_.fullname -newname ($newName) }} -Verbose; Get-ChildItem '* (1).*'| Remove-Item -Verbose;
