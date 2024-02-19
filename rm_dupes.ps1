809003abd21270eab2454f29b335fe148e0cfb47
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-19 16:08:15 -0600

    add rm dupes

---
+++ rm_dupes.ps1
@@ -0,0 +1,4 @@
+Get-ChildItem -Recurse | ForEach-Object {
+		$NewName = $_.Name -replace ’ \(1\)’,’’
+		if (-not ($_.name -eq $newname)){
+			   Rename-Item -Path $_.fullname -newname ($newName) }} -Verbose; Get-ChildItem ’* (1).*’| Remove-Item -Verbose;
