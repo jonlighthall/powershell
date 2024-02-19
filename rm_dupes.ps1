c4b7c13e89cf95cf9e3db9ec9ac7ad35e0e9f0a0
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-19 16:15:47 -0600

    format rm dupes

--- rm_dupes.ps1
+++ rm_dupes.ps1
@@ -1,4 +1,5 @@
 Get-ChildItem -Recurse | ForEach-Object {
-		$NewName = $_.Name -replace ' \(1\)',''
-		if (-not ($_.name -eq $newname)){
-			   Rename-Item -Path $_.fullname -newname ($newName) }} -Verbose; Get-ChildItem '* (1).*'| Remove-Item -Verbose;
+	$NewName = $_.Name -replace ' \(1\)', ''
+	if (-not ($_.name -eq $newname)) {
+		Rename-Item -Path $_.fullname -newname ($newName) 
+	} } -Verbose; Get-ChildItem '* (1).*' | Remove-Item -Verbose;
