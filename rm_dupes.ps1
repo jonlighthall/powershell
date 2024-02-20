6b125fcd7d2bdc563e76cf41b7d60f340d47e0f8
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-19 20:41:32 -0600

    format, add color, fix commands

--- rm_dupes.ps1
+++ rm_dupes.ps1
@@ -1,22 +1,26 @@
 $files = Get-ChildItem -Recurse | Where-Object { $_.Name -match "\(2\)" }
 
 foreach ($file in $files) {
+	Write-Host " input file name: $file"
 	$originalName = $file.Name -replace ' \(2\)', ''
-	$originalFile = Get-ChildItem -Path $file.Directory.FullName | Where-Object { $_.Name -eq $originalName }
-	Write-Output $file.Name
-	Write-Output $originalName
-	#Write-Output $originalFile
+	Write-Host "output file name: $originalName"
 
-	if ($originalFile) {
-		write-host "$originalFile exists"
+	Write-Host " input file:" $file.FullName
+	$outFileName= Join-Path -Path $file.Directory.FullName -ChildPath $originalName
+	Write-Host "output file: $outFileName... " -NoNewline
+
+	if (Test-Path -Path $outFileName -PathType Leaf) {
+		Write-Host "already exists, " -ForegroundColor Red -NoNewline
 		if ($originalFile.Length -eq $file.Length) {
-			write-host "files have same length"
+			write-host "files have same length" -ForegroundColor Yellow
+			Remove-Item -Path $file.FullName -Verbose
 		}
 		else {
-			write-host "files have different length"
+			write-host "files have different lengths" -ForegroundColor Red
 		}
 	}
 	else {
-		write-host "$originalFile does not exist"
+		write-host "$originalFile does not exist" -ForegroundColor Green
+		Move-Item -Path $file.FullName -Destination $outFileName -Verbose
 	}
 }
