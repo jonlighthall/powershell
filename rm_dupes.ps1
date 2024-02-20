0444b646a460156418c75f0c0f548ac8634fab3a
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-19 20:28:26 -0600

    expand logic

--- rm_dupes.ps1
+++ rm_dupes.ps1
@@ -1,12 +1,22 @@
-$files = Get-ChildItem -Recurse | Where-Object { $_.Name -match ' \(2\)$' }
+$files = Get-ChildItem -Recurse | Where-Object { $_.Name -match "\(2\)" }
 
 foreach ($file in $files) {
-	$originalName = $file.Name -replace ' \(2\)$', ''
+	$originalName = $file.Name -replace ' \(2\)', ''
 	$originalFile = Get-ChildItem -Path $file.Directory.FullName | Where-Object { $_.Name -eq $originalName }
+	Write-Output $file.Name
+	Write-Output $originalName
+	#Write-Output $originalFile
 
-	if ($originalFile -and $originalFile.Length -eq $file.Length) {
-		Remove-Item -Path $file.FullName -Verbose
-	} elseif (-not $originalFile) {
-		Rename-Item -Path $file.FullName -NewName $originalName -Verbose
+	if ($originalFile) {
+		write-host "$originalFile exists"
+		if ($originalFile.Length -eq $file.Length) {
+			write-host "files have same length"
+		}
+		else {
+			write-host "files have different length"
+		}
+	}
+	else {
+		write-host "$originalFile does not exist"
 	}
 }
