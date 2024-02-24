f4a2886c1de698d692e2ee68636cd3a8c2f5b25b
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-24 15:32:21 -0600

    add link profile

--- make_links.ps1
+++ make_links.ps1
@@ -27,30 +27,32 @@ $target = (Get-Item $cloud)
 $local = 'C:\Users\jonli\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt'
 $link = (Get-Item $local)
 
+$TAB="   "
+
 # check if the source file exists
 Write-Host "test cloud history..."
 if (Test-Path -Path $target) {
-	Write-Host "   $target found"
+	Write-Host "${TAB}$target found"
 	Write-Host "proceeding..."
 }
 else {
-	Write-Host "   $target not found" -ForegroundColor Red
-	Write-Host "   no history to link to`nexiting"
+	Write-Host "${TAB}$target not found" -ForegroundColor Red
+	Write-Host "${TAB}no history to link to`nexiting"
 	exit 1
 }
 
 Write-Host "test local history..."
 $do_append = $false
 if (Test-Path -Path $local) {
-	Write-Host "   $local found"
+	Write-Host "${TAB}$local found"
 	$do_append = $true
 
-	Write-Host "   $link is... " -NoNewline
+	Write-Host "${TAB}$link is... " -NoNewline
 	$do_link = $false
 
 	if ($link.LinkType -eq "SymbolicLink") {
 		Write-Host "a link "
-		Write-Host "   and points to... " -NoNewline
+		Write-Host "${TAB}and points to... " -NoNewline
 		$linkTarget = (Get-Item -Path $local).Target
 		if ($linkTarget -eq $cloud) {
 			Write-Host "$cloud"
@@ -83,25 +85,66 @@ else {
 
 if ($do_link) {
 	if ($do_append) {
-		Write-Host "   proceeding with append, delete, and link..."
-		Write-Host "      appending local copy with cloud copy..."
+		Write-Host "${TAB}proceeding with append, delete, and link..."
+		Write-Host "${TAB}${TAB}appending local copy with cloud copy..."
 		Add-Content -Path $cloud -Value $local
 
-		Write-Host "      renaming local copy"
+		Write-Host "${TAB}${TAB}renaming local copy"
 		$dir = [io.path]::GetDirectoryName($local)
 		$fname = [io.path]::GetFileNameWithoutExtension($local)
 		$ext = [io.path]::GetExtension($local)
 		Move-Item -v $local $dir\${fname}_$(get-date -f yyyy-MM-dd-hhmm)$ext
 	}
 	else {
-		Write-Host "   no appending to do"
-		Write-Host "   proceeding with link..."
+		Write-Host "${TAB}no appending to do"
+		Write-Host "${TAB}proceeding with link..."
 	}
-	Write-Host "      creating symbolic link"
+	Write-Host "${TAB}${TAB}creating symbolic link"
 	cmd /c mklink $local $cloud
 }
 else {
-	Write-Host "   no linking to do"
+	Write-Host "${TAB}no linking to do"
 }
+write-host "done linking history"
+
+Write-Host "List of profiles:"
+$PROFILE | Format-List -Force
+
+Write-Host "Current profile: ......... $profile"
+write-host "Current user current host: " -noNewline
+$cuch_profile=$PROFILE.CurrentUserCurrentHost
+$cuch_profile
+
+Write-Host "Current profile... " -NoNewline
+if (Test-Path -Path $PROFILE.CurrentUserCurrentHost) {
+	Write-Host "exists"
+
+	Write-Host "${TAB}${TAB}renaming local copy"
+		$dir = [io.path]::GetDirectoryName($cuch_profile)
+		$fname = [io.path]::GetFileNameWithoutExtension($cuch_profile)
+		$ext = [io.path]::GetExtension($cuch_profile)
+		Move-Item -v $cuch_profile $dir\${fname}_$(get-date -f yyyy-MM-dd-hhmm)$ext
+
+}
+else {
+	Write-Host "does not exist"
+}
+
+$myProfile = Join-Path -Path $src_path -ChildPath "Profile.ps1"
+
+if (Test-Path -Path $myProfile) {
+	Write-Host "${TAB}${TAB}copying profile"
+
+	New-Item -ItemType SymbolicLink -Path $cuch_profile -Target $myProfile
+}
+else {
+	Write-Host "${TAB}${TAB}profile not found" -ForegroundColor Red
+	Write-Host "${TAB}${TAB}no profile to link to`nexiting"
+	exit 1
+}
+
+Write-Host "Linking profile... "
+
+write-host "done linking profile"
 
 write-host "done"
