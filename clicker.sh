f421238f9c2c26077f228dc7f0dca6dc097ab5d2
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-14 13:21:19 -0500

    replace NORMAL with RESET

--- clicker.sh
+++ clicker.sh
@@ -18,7 +18,7 @@ fi
 echo "${TAB}${RUN_TYPE} $BASH_SOURCE..."
 src_name=$(readlink -f $BASH_SOURCE)
 if [ ! "$BASH_SOURCE" = "$src_name" ]; then
-  echo -e "${TAB}${VALID}link${NORMAL} -> $src_name"
+  echo -e "${TAB}${VALID}link${RESET} -> $src_name"
 fi
 
 # get source directory
