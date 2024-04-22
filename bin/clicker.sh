bf22a520729e2acb9cf891902abbcacfdff773a0
Jon Lighthall <jon.lighthall@gmail.com>
2024-04-22 11:39:41 -0500

    rename scrlk.ps1

--- bin/clicker.sh
+++ bin/clicker.sh
@@ -15,11 +15,7 @@ if (return 0 2>/dev/null); then
 else
   RUN_TYPE="executing"
 fi
-echo "${TAB}${RUN_TYPE} $BASH_SOURCE..."
-src_name=$(readlink -f $BASH_SOURCE)
-if [ ! "$BASH_SOURCE" = "$src_name" ]; then
-  echo -e "${TAB}${VALID}link${RESET} -> $src_name"
-fi
+print_source
 
 # get source directory
 source_dir=$(dirname "$src_name")
@@ -27,5 +23,5 @@ cd $source_dir
 echo -n "connecting to powershell... "
 powershell.exe Write-Output "done"
 clear -x
-powershell.exe -File ../scrlk.ps1
+powershell.exe -File ../clicker.ps1
 trap "echo -e '\x1B[11G BASH: $(sec2elap $SECONDS)';clear -x" EXIT
