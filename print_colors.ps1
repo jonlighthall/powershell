c81f8b6d139fd2e80c231e1e86cad8e6f6aa332f
Jon Lighthall <jon.lighthall@gmail.com>
2024-07-19 21:09:19 -0500

    add print colors

---
+++ print_colors.ps1
@@ -0,0 +1,23 @@
+
+$colors = @(
+    "Black",
+    "DarkBlue",
+    "DarkGreen",
+    "DarkCyan",
+    "DarkRed",
+    "DarkMagenta",
+    "DarkYellow",
+    "Gray",
+    "DarkGray",
+    "Blue",
+    "Green",
+    "Cyan",
+    "Red",
+    "Magenta",
+    "Yellow",
+    "White"
+)
+
+foreach ($color in $colors) {
+    Write-Host "$color" -ForegroundColor $color
+}
