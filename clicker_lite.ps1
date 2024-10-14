2a64fccacc7ae34245ecbb25148fc7a759ee5323
Jon Lighthall <jon.lighthall@gmail.com>
2024-10-14 11:49:15 -0500

    [COPILOT] further reduction in size, remove variables

--- clicker_lite.ps1
+++ clicker_lite.ps1
@@ -1,14 +1,9 @@
-$loop_wait_min = 4
-$blink_wait_ms = 32
-$blinks_per_loop = 2
-$keys = @("{CAPSLOCK}")
-$loop_wait_ms = $loop_wait_min * 60 * 1000
 $WShell = New-Object -ComObject Wscript.Shell
 Write-Host "Press Ctrl-C to exit."
 while ($true) {
-    1..($blinks_per_loop * 2) | ForEach-Object {
-        $keys | ForEach-Object { $WShell.sendkeys($_) }
-        Start-Sleep -Milliseconds $blink_wait_ms
+    1..4 | ForEach-Object {
+        $WShell.sendkeys("{CAPSLOCK}")
+        Start-Sleep -Milliseconds 32
     }
-    Start-Sleep -Milliseconds (Get-Random -Minimum 0 -Maximum $loop_wait_ms)
+    Start-Sleep -Milliseconds (Get-Random -Minimum 0 -Maximum 240000)
 }
