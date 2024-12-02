12cec3027ca2f9f6cab7399204a742120d9ac872
Jon Lighthall <jon.lighthall@gmail.com>
2024-12-02 11:38:29 -0600

    add comments

--- clicker_lite.ps1
+++ clicker_lite.ps1
@@ -1,10 +1,12 @@
 $WShell = New-Object -ComObject Wscript.Shell
 Write-Host "Press Ctrl-C to exit."
 while ($true) {
-
+    # the following loop will toggle the caps lock key 4 times, making the
+    # indicator light blink twice
     1..4 | ForEach-Object {
         $WShell.sendkeys("{CAPSLOCK}")
         Start-Sleep -Milliseconds 32
     }
+    # the script will sleep for a random amount of time between 0 and 4 minutes
     Start-Sleep -Milliseconds (Get-Random -Minimum 0 -Maximum 4*60*1000)
 }
--- clicker_time.ps1
+++ clicker_time.ps1
@@ -1,13 +1,17 @@
 $WShell = New-Object -ComObject Wscript.Shell
 Write-Host "Press Ctrl-C to exit."
 while ($true) {
-    if ((Get-Date).Hour -ge 11) {
+# enter the time you want the script to stop (24-hour format)
+    if ((Get-Date).Hour -ge 17) {
         write-host "$((Get-Date).Hour):$((Get-Date).Minute) done"
         break
     }
+    # the following loop will toggle the caps lock key 4 times, making the
+    # indicator light blink twice
     1..4 | ForEach-Object {
         $WShell.sendkeys("{CAPSLOCK}")
         Start-Sleep -Milliseconds 32
     }
+    # the script will sleep for a random amount of time between 0 and 4 minutes
     Start-Sleep -Milliseconds (Get-Random -Minimum 0 -Maximum 4*60*1000)
 }
