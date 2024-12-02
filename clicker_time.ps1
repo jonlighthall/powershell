bc940a1b14fefcfa461221140758323d60aea501
Jon Lighthall <jon.lighthall@gmail.com>
2024-12-02 11:33:59 -0600

    add clicker with time

--- clicker_time.ps1
+++ clicker_time.ps1
@@ -1,7 +1,10 @@
 $WShell = New-Object -ComObject Wscript.Shell
 Write-Host "Press Ctrl-C to exit."
 while ($true) {
-
+    if ((Get-Date).Hour -ge 11) {
+        write-host "$((Get-Date).Hour):$((Get-Date).Minute) done"
+        break
+    }
     1..4 | ForEach-Object {
         $WShell.sendkeys("{CAPSLOCK}")
         Start-Sleep -Milliseconds 32
